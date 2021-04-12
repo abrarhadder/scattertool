7912f06734e1f94c0b6b02422e0393d0b5cf7b2e
abrarhadder <79809868+abrarhadder@users.noreply.github.com>
2021-04-12 15:31:07 -0500

    Create scatter.py
    Initialize project libraries

---
+++ scatter.py
@@ -0,0 +1,9 @@
+import logging
+
+from PySide2 import QtWidgets, QtCore
+from shiboken2 import wrapInstance
+import maya.OpenMayaUI as omui
+import maya.cmds as cmds
+import pymel.core as pmc
+from pymel.core.system import Path
+import os
